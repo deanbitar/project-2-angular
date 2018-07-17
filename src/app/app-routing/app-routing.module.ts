b959c8e323ca608c22d6bf941335d01e458e7916
Eric Stefanko <ericrstefanko@gmail.com>
2018-07-17 19:32:51 -0400

    added paths to routing module

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from '../login/login.component';
 import { ForgotPasswordComponent } from '../forgot-password/forgot-password.component';
+import { HomeComponent } from '../home/home.component';
+import { RegisterComponent } from '../register/register.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'forgot-password', component: ForgotPasswordComponent }
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'register', component: RegisterComponent }
 ];
 
 @NgModule({
