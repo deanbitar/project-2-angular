dbe24a465f662e27aba5735d96c3f01770c36781
nmismail <najibismail95@gmail.com>
2018-07-22 10:36:31 -0400

    updates

--- src/app/shared/http.service.ts
+++ src/app/shared/http.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { User } from './user';
 const httpOptions = {
-   headers: new HttpHeaders({ 'Content-Type': 'x-www-form-urlencoded'
+   headers: new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded',
    })
 };
 
@@ -18,9 +18,6 @@ export class HttpService {
   }
 
   public loginUser(inEmail: string, inPassword: string): Observable<string> {
-    // const body = new HttpParams();
-    // body.set('email', inEmail);
-    // body.set('password', inPassword);
     const path = this.url.concat('login.chord');
     return this.http.post(path, {email: inEmail, password: inPassword}, httpOptions).pipe(map(resp => resp as string));
   }
