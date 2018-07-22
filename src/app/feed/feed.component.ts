9ddba317c854654f07c64a48a294b7ad01dc0664
amilevich <alexandra.i.milevich@gmail.com>
2018-07-22 14:56:44 -0400

    started working on feed data binding

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../shared/User';
 
 @Component({
   selector: 'app-feed',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedComponent implements OnInit {
 
+  users: User[] = [];
+
   constructor() { }
+
+
 
   ngOnInit() {
   }
