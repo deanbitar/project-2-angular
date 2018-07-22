4f9eade3b115b7668eebdd35b74789946b26cebc
Eric Stefanko <ericrstefanko@gmail.com>
2018-07-22 18:42:23 -0400

    Profile Stuff

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -11,17 +11,18 @@ import { Post } from '../shared/post';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-  private user: User;
-  private posts: Post [];
+  user: User;
+  posts: Post[];
 
   constructor(private route: ActivatedRoute, private httpService: HttpService) { }
 
   ngOnInit() {
     const userId = Number(this.route.snapshot.paramMap.get('userId'));
 
-      this.httpService.getUserById(userId)
-        .subscribe(user => this.parseUser(user));
-        this.httpService.getUserFeed(userId).subscribe(data => this.parsePosts(data));
+    this.httpService.getUserById(userId)
+      .subscribe(user => this.parseUser(user));
+    this.httpService.getUserFeed(userId).subscribe(data => this.parsePosts(data));
+    this.posts.sort((a, b) => new Date(b.submitTime).getTime() - new Date(a.submitTime).getTime());
   }
 
   parseUser(user) {
@@ -30,5 +31,12 @@ export class ProfileComponent implements OnInit {
 
   parsePosts(data) {
     this.posts = data;
+  }
+
+  getDate(inDate: number) {
+    const date = new Date(inDate);
+    return date;
+    /*     return date.getHours() + 1; */
+
   }
 }
